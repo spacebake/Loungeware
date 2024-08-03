b099c1ca9397618e760e64812317bc39c7219da1
net <net8floz@gmail.com>
2024-08-03 15:29:31 -0700

    Update stitch_prepare_loungeware.bat

--- stitch_prepare_loungeware.bat
+++ stitch_prepare_loungeware.bat
@@ -10,8 +10,8 @@ if %errorlevel% equ 0 (
   curl  -L -o nvm-setup.exe https://github.com/coreybutler/nvm-windows/releases/download/1.1.11/nvm-setup.exe
   start /w "node setup" nvm-setup.exe
   del nvm-setup.exe
-  setx NVM_HOME "%USERPROFILE%\.nvm"
-  setx PATH "%PATH%;%USERPROFILE%\.nvm"
+  REM setx NVM_HOME "%USERPROFILE%\.nvm"
+  REM setx PATH "%PATH%;%USERPROFILE%\.nvm"
   nvm install 16
   nvm use 16
 )
